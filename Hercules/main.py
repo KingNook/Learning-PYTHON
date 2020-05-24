02b0c72aa68f76f6dd0a34eea0f9f744e95a59f8
KingNook <mike04.lin@gmail.com>
2020-05-24 17:29:50 +0100

    filled in labours

---
+++ Hercules/main.py
@@ -0,0 +1,13 @@
+# 1 - Nemean Lion
+# 2 - Lernaean Hydra
+# 3 - Ceryneian Hind (Artemis Deer)
+# 4 - Erymanthian Boar
+#*5 - Augean Stables (Clean / Rivers)
+# 6 - Stymphalian Birds
+# 7 - Cretan Bull (Capture)
+# 8 - Mares of Diomedes (Man-eating horses)
+# 9 - Girdle of Hippolyta
+# 10 - Cattle of Geryon (3 - bodied giant)
+
+# 11 - Apples of the Hespirides (Golden Apples / Atlas)
+# 12 - Capture Cerberus
