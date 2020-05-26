397bf67107b1e19ff79f5f66dc6a1fd9b9b3341d
KingNook <mike04.lin@gmail.com>
2020-05-26 18:46:29 +0100

    clean up 25 - 43

--- Game of Life/sim.py
+++ Game of Life/sim.py
@@ -1,5 +1,52 @@
 import numpy
 
 class Grid:
-    def __init__(self, x_dim, y_dim):
-        self.grid = numpy.zeros((x_dim, y_dim), dtype=bool)
+    def __init__(self, x_dim, y_dim, grid = None):
+        self.x = x_dim
+        self.y = y_dim
+        self.grid = grid if grid else numpy.zeros((x_dim, y_dim), dtype=bool)
+
+    def set_grid(self, grid):
+        '''
+        leave <grid> argument empty to reset grid
+        '''
+        if grid:
+            self.grid = grid
+            return True
+        else:
+            self.grid = numpy.zeros((self.x, self.y), dtype=bool)
+            return None
+
+    def surrounding_cells(self, cell_x, cell_y):
+        cells = []
+        # Left, Right, Top, Bottom
+        sides = [cell_x != 0, cell_x != self.x, cell_y != 0, cell_y != self.y]
+        # Clean up this code
+        if sides[0]:
+            cells.append((self.x-1, self.y))
+        else:
+            cells.append(None)
+        
+        if sides[1]:
+            cells.append((self.x+1, self.y))
+        else:
+            cells.append(None)
+        
+        if sides[2]:
+            cells.append((self.x, self.y-1))
+        else:
+            cells.append(None)
+        
+        if sides[3]:
+            cells.append((self.x, self.y+1))
+        else:
+            cells.append(None)
+
+    def update(self):
+        '''
+        One in-game tick (update board)
+        '''
+        prev = self.grid.copy()
+        for i in range(self.x):
+            for j in range(self.y):
+                pass
