0b87ff3860f62e13a2588237c72eb47cf7175230
KingNook <mike04.lin@gmail.com>
2020-05-27 10:56:44 +0100

    working viewer at 2fps

--- Game of Life/viewer.py
+++ Game of Life/viewer.py
@@ -1,8 +1,39 @@
-# dimensions for pygame viewer
-DIMENSIONS = (100, 100)
-
 import pygame
+from sim import Grid, gg
+
+g = Grid(100)
+
+# dimensions for pygame viewer
+DIMENSIONS = (550, 550)
+PIX = (5, 5)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 pygame.init()
 
 screen = pygame.display.set_mode(DIMENSIONS)
+
+clock = pygame.time.Clock()
+
+gg(g)
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill(WHITE)
+
+    for i in range(g.x):
+        for j in range(g.y):
+            if g.get_cell(j, i):
+                pygame.draw.rect(screen, BLACK, pygame.Rect(i*PIX[0]+25, j*PIX[1]+25, PIX[0], PIX[1]))
+
+    pygame.display.flip()
+    
+    g.update()
+    clock.tick(2)
+
+pygame.quit()
