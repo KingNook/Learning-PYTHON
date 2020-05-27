d82a1fc323674469af41e6e6a9880fd014d98638
KingNook <mike04.lin@gmail.com>
2020-05-27 11:00:04 +0100

    add nice border

--- Game of Life/viewer.py
+++ Game of Life/viewer.py
@@ -8,10 +8,14 @@ DIMENSIONS = (550, 550)
 PIX = (5, 5)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BORDER = (128, 128, 128)
 
 pygame.init()
 
 screen = pygame.display.set_mode(DIMENSIONS)
+active_screen = pygame.Rect(25, 25, 500, 500)
+# Set border to gray
+screen.fill(BORDER)
 
 clock = pygame.time.Clock()
 
@@ -24,7 +28,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill(WHITE)
+    pygame.draw.rect(screen, WHITE, active_screen)
 
     for i in range(g.x):
         for j in range(g.y):
