94a7955fb0d661561a777ab571f86ad02a91b347
KingNook <mike04.lin@gmail.com>
2020-05-31 13:51:09 +0100

    add parse, condense peices

--- Chess/chess.py
+++ Chess/chess.py
@@ -1,33 +1,39 @@
 import pygame
 
 class Board:
-    def __init__(self, position = None):
-        self.white = {}
-        self.black = {}
+    def __init__(self, display, position = None):
+        self.pieces = {}
+        self.board = []
+        self.display = display
 
         self.get_pieces()
 
         # Starting position
         self.fen = position if position else 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        self.parse()
 
     def get_pieces(self):
         # White pieces
-        self.white['K'] = pygame.image.load('/pieces/Chess_klt60.png')
-        self.white['Q'] = pygame.image.load('/pieces/Chess_qlt60.png')
-        self.white['R'] = pygame.image.load('/pieces/Chess_rlt60.png')
-        self.white['B'] = pygame.image.load('/pieces/Chess_blt60.png')
-        self.white['N'] = pygame.image.load('/pieces/Chess_nlt60.png')
-        self.white['P'] = pygame.image.load('/pieces/Chess_plt60.png')
+        self.pieces['k'] = pygame.image.load('/pieces/Chess_klt60.png')
+        self.pieces['q'] = pygame.image.load('/pieces/Chess_qlt60.png')
+        self.pieces['r'] = pygame.image.load('/pieces/Chess_rlt60.png')
+        self.pieces['b'] = pygame.image.load('/pieces/Chess_blt60.png')
+        self.pieces['n'] = pygame.image.load('/pieces/Chess_nlt60.png')
+        self.pieces['p'] = pygame.image.load('/pieces/Chess_plt60.png')
 
         # Black pieces
-        self.black['K'] = pygame.image.load('/pieces/Chess_kdt60.png')
-        self.black['Q'] = pygame.image.load('/pieces/Chess_qdt60.png')
-        self.black['R'] = pygame.image.load('/pieces/Chess_rdt60.png')
-        self.black['B'] = pygame.image.load('/pieces/Chess_bdt60.png')
-        self.black['N'] = pygame.image.load('/pieces/Chess_ndt60.png')
-        self.black['P'] = pygame.image.load('/pieces/Chess_pdt60.png')
+        self.pieces['K'] = pygame.image.load('/pieces/Chess_kdt60.png')
+        self.pieces['Q'] = pygame.image.load('/pieces/Chess_qdt60.png')
+        self.pieces['R'] = pygame.image.load('/pieces/Chess_rdt60.png')
+        self.pieces['B'] = pygame.image.load('/pieces/Chess_bdt60.png')
+        self.pieces['N'] = pygame.image.load('/pieces/Chess_ndt60.png')
+        self.pieces['P'] = pygame.image.load('/pieces/Chess_pdt60.png')
 
         return True
 
-    def parse(self):
-        return NotImplemented
+    def parse(self, fen = None):
+        # Use default starting position if none provided
+        fen = fen if fen else self.fen
+
+        pos, move, castle, en_passant, halfmove, fullmove = fen.split(' ')
+        self.board = [list(i) for i in pos.split('/')]
