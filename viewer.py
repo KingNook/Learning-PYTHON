dfaf987fdd245987f1c9c987d69cd799b948f9b0
KingNook <mike04.lin@gmail.com>
2020-05-31 11:55:11 +0100

    -

--- viewer.py
+++ viewer.py
@@ -1,5 +1,11 @@
 import pygame
+pygame.init()
 
 class PyViewer:
     def __init__(self, dim):
         self.screen = pygame.display.set_mode(dim)
+
+
+class Memoize:
+    def __init__(self):
+        pass
