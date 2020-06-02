7a8f6b2aaa4ba2d370d12efdf027400fcbac2ab3
KingNook <mike04.lin@gmail.com>
2020-06-02 16:07:14 +0100

    begin piece classes

--- Chess/pieces.py
+++ Chess/pieces.py
@@ -4,3 +4,21 @@ pygame.init()
 class Piece:
     def __init__(self, icon):
         self.icon = icon
+
+class King:
+    pass
+
+class Queen:
+    pass
+
+class Rook:
+    pass
+
+class Bishop:
+    pass
+
+class Knight:
+    pass
+
+class Pawn:
+    pass
