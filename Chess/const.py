a665e6866720e7c1c36b1b769eb61bfc63e39b93
KingNook <mike04.lin@gmail.com>
2020-06-03 20:47:13 +0100

    Add piece map to const
    *IMPORTANT* keys are lower case

--- Chess/const.py
+++ Chess/const.py
@@ -1,3 +1,8 @@
+import pieces
+
+DIM = (480, 480)
+PIX = (DIM[0] // 8, DIM[1] // 8)
+
 LIGHT = (255, 251, 184)
 DARK = (0, 67, 128)
 
@@ -10,5 +15,11 @@ COL = {'a': '0', 'b': '1', 'c': '2', 'd': '3', 'e': '4', 'f': '5', 'g': '6', 'h'
 
 PIECES = {'l': 'kqrbnp', 'd': 'KQRBNP'}
 
-DIM = (480, 480)
-PIX = (DIM[0] // 8, DIM[1] // 8)
+PIECE_MAP = {
+    'k' : pieces.King,
+    'q' : pieces.Queen,
+    'r' : pieces.Rook,
+    'b' : pieces.Bishop,
+    'n' : pieces.Knight,
+    'p' : pieces.Pawn
+}
