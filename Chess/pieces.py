39dc24cbd77784b9b2fb1dea84d8d8fb762d2df8
KingNook <mike04.lin@gmail.com>
2020-06-05 09:43:17 +0100

    -

--- Chess/pieces.py
+++ Chess/pieces.py
@@ -36,8 +36,7 @@ class King(Piece):
         super().__init__(icon, board)
 
     # Moves a King can make from position (a, b)
-    @staticmethod
-    def moves(a, b, c):
+    def moves(self, a, b, c):
         # Available moves
         m = [(i, j) for i in range(-1, 2) for j in range(-1, 2) if not i == j == 0]
 
@@ -47,47 +46,51 @@ class King(Piece):
             state = super().valid_square(move, c)
             if state != -1:
                 valid_moves.append(move)
+
+        return valid_moves
         
 
 class Queen(Piece):
     def __init__(self, icon, board):
         super().__init__(icon, board)
 
+    def valid_square(self, coord, color):
+        return super().valid_square(coord, color)
+
     # Moves a queen of color c [string 'l' or string 'd'] can make from position (a, b)
-    @staticmethod
-    def moves(a, b, c):
+    def moves(self, a, b, c):
         m = []
         
         x = a - 1
         # Horizontally to left
-        while super().valid_square((x, b), c) == 0:
+        while self.valid_square((x, b), c) == 0:
             m.append((x, b))
             x -= 1
-        if super().valid_square((x, b), c) == 1:
+        if self.valid_square((x, b), c) == 1:
             m.append((x, b))
 
         # Horizontally to right
         x = a + 1
-        while super().valid_square((x, b), c) == 0:
+        while self.valid_square((x, b), c) == 0:
             m.append((x, b))
             x += 1
-        if super().valid_square((x, b), c) == 1:
+        if self.valid_square((x, b), c) == 1:
             m.append((x, b))
 
         # Vertically upwards
         y = b - 1
-        while super().valid_square((a, y), c) == 0:
+        while self.valid_square((a, y), c) == 0:
             m.append((a, y))
             x -= 1
-        if super().valid_square((a, y), c) == 1:
+        if self.valid_square((a, y), c) == 1:
             m.append((a, y))
         
         # Vertically downwards
         y = b - 1
-        while super().valid_square((a, y), c) == 0:
+        while self.valid_square((a, y), c) == 0:
             m.append((a, y))
             x -= 1
-        if super().valid_square((a, y), c) == 1:
+        if self.valid_square((a, y), c) == 1:
             m.append((a, y))
 
         return m
--- Chess/viewer.py
+++ Chess/viewer.py
@@ -6,6 +6,10 @@ pygame.display.set_caption('Chess')
 screen = pygame.display.set_mode(const.DIM)
 
 board = Board(screen)
+
+# Remove after debugging
+board.parse('00000000/00000000/0000q000/00000000/00000000/00000000/00000000/00000000 w KQkq - 0 1')
+
 board.draw_board()
 
 # GAME LOOP
